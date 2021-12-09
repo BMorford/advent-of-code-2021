15a88299316e86895c7abc8b601a397a098e6efe
bmorford <brmorford@gmail.com>
2021-12-08 20:00:20 -0700

    init

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Part 1
+int[] input = Array.ConvertAll(File.ReadAllLines("./input.txt"), int.Parse);
+
+// This is just for fun, it's bad though so don't actually use linq for this.
+int result = input.Skip(1).Zip(input, (x, y) => x > y ? 1 : 0).Sum();
+
+Console.WriteLine(result);
+
+
+// Part 2
+static int CalculateIncreasingWindowCount (int[] input, int windowSize)
+{
+    int currentSum = 0;
+    int count = 0;
+
+    for (int i = 0; i < windowSize; i++)
+    {
+        currentSum += input[i];
+    }
+
+    int windowSum = currentSum;
+    for (int i = windowSize; i < input.Length; i++)
+    {
+        windowSum += input[i] - input[i - windowSize];
+        if (windowSum > currentSum)
+        {
+            count++;
+        }
+        currentSum = windowSum;
+    }
+    return count;
+}
+
+Console.WriteLine(CalculateIncreasingWindowCount(input, 3));
