07d3ea85113ca7e91501f8899aad48e8d82b5be9
Brandon.Morford <brandon.morford@blackbaud.me>
2021-12-21 15:22:40 -0700

    Add day 3 part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] input = File.ReadAllLines("./input.txt");
+
+// Part 1
+decimal CalculateGammaRate(string[]input)
+{
+    int itemLength = input.First().Length;
+    int inputLength = input.Length;
+    IEnumerable<int> gammaRate = input.SelectMany(x => x)
+        .Select((x, i) => new { Value = x, Index = i % itemLength })
+        .GroupBy(x => x.Index)
+        .Select(g => g
+            .Sum(z => char.GetNumericValue(z.Value)) < inputLength / 2 ? 0 : 1);
+
+    return Convert.ToInt32(string.Join("", gammaRate), 2);
+}
+
+decimal gammaRate = CalculateGammaRate(input);
+uint epsilonRate = (uint)gammaRate ^ 0xFFF;
+Console.WriteLine(gammaRate * epsilonRate);
